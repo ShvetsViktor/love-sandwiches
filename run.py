a1956964eaa96a9b06bafb532047a063dafc75fa
Viktor <shvetsviktor89@gmail.com>
2026-09-16 11:58:56 +0100

    Add calculated surplus data to the surplus worksheet.

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
 import gspread
 from google.oauth2.service_account import Credentials
-# from pprint import pprint
 
 
 SCOPE = [
@@ -45,7 +44,8 @@ def get_sales_data():
 def validate_data(values):
     """
     Inside the try, converts all string values into integers.
-    Rases ValueError if strings cannot be converted into integers or if there are not exactly 6 values.
+    Rases ValueError if strings cannot be converted into integers or if there
+    are not exactly 6 values.
     """
     try:
         [int(value) for value in values]
@@ -70,6 +70,16 @@ def update_sales_worksheet(data):
     print("Sales worksheet updated successfully\n")
 
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully\n")
+
+
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -89,12 +99,13 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+
 def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
     new_surplus_data = calculate_surplus_data(sales_data)
-    print(new_surplus_data)
+    update_surplus_worksheet(new_surplus_data)
 
 
 print("Welcome to LoveSandwiches Data Automation")
